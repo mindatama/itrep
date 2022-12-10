dba0a094c36102cf2cc77403148e6d1b315f0e8e
mindatama <mindatama@gmail.com>
2022-12-09 22:04:20 -1200

    cuma commentaja

--- lib/pages/home/network_page.dart
+++ lib/pages/home/network_page.dart
@@ -12,7 +12,7 @@ class NetworkPage extends StatelessWidget {
         backgroundColor: Colors.transparent,
         centerTitle: true,
         title: Text(
-          'Networks aja kasih grid joss',
+          'Networks aja kasih grid jossss',
           style: primaryTextStyle.copyWith(fontSize: 18, fontWeight: medium),
         ),
         elevation: 0,
