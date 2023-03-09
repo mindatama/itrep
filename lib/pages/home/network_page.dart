17c3dc43e3414b9a71dddd34bd05ee2bd7b00523
mindatama <mindatama@gmail.com>
2023-03-09 22:01:07 +0800

    diluar code

--- lib/pages/home/network_page.dart
+++ lib/pages/home/network_page.dart
@@ -24,4 +24,4 @@ class NetworkPage extends StatelessWidget {
   }
 }
 
-//berkutat serer dulu ya sehari ini, lnjut 2hr, lnjut hr.. lanjut docker
+//berkutat serer dulu ya sehari ini, lnjut 2hr, lnjut hr.. lanjut docker..
