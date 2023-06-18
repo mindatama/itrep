4c3f7345cf473ef8f8c934b109e768fa92a43c62
mindatama <mindatama@gmail.com>
2023-06-18 09:06:50 +0800

    tambah comment aja, ngejar 18 jun kerjaan dl di office

--- lib/pages/home/network_page.dart
+++ lib/pages/home/network_page.dart
@@ -26,6 +26,7 @@ class NetworkPage extends StatelessWidget {
 //ngejar 15 jun masih devops
 //ngejar 16 jun masih devops (nutanix)
 //ngejar 17 jun belum nge progress apa apa
+//ngejar 18 jun kerja dulu
 //masih lanjut docker lagi
 //lagi, lagi, lagi, lagi, lagi.lagi
 //lagi, sekarang fokus nutanix.. lagi. membukalagi, lagi. lagi, lagi. lagi, lagi
