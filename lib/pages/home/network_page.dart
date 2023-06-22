00ba94ad6dbcbb898c7d3c257f566dc6ac92f91f
mindatama <mindatama@gmail.com>
2023-06-21 23:27:12 -0400

    tambah comment aja, ngejar 21 jun lupa buat

--- lib/pages/home/network_page.dart
+++ lib/pages/home/network_page.dart
@@ -29,6 +29,7 @@ class NetworkPage extends StatelessWidget {
 //ngejar 18 jun kerja dulu
 //ngejar 19 jun ikut lomba, menang cuy juara 2
 //20 jun mulai nyatet nutanix, bismillah
+//21 jun lupa buat
 //masih lanjut docker lagi
 //lagi, lagi, lagi, lagi, lagi.lagi
 //lagi, sekarang fokus nutanix.. lagi. membukalagi, lagi. lagi, lagi. lagi, lagi
