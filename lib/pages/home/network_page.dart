1d8189c801c5673826d9c145afbe5f6015389fb2
mindatama <mindatama@gmail.com>
2023-06-29 23:33:09 -1200

    tambah comment aja, 28 jun malah scoll

--- lib/pages/home/network_page.dart
+++ lib/pages/home/network_page.dart
@@ -37,6 +37,7 @@ class NetworkPage extends StatelessWidget {
 //26 jun kaga ngapa2in, asem
 //27 jun scroll aja,hah
 //28 jun keknya bisa deh
+//29 jun malah scroll
 //masih lanjut docker lagi
 //lagi, lagi, lagi, lagi, lagi.lagi
 //lagi, sekarang fokus nutanix.. lagi. membukalagi, lagi. lagi, lagi. lagi, lagi
