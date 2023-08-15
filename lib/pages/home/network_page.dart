db48a6c559865cf91267618f80ddd2117ae35d5f
mindatama <mindatama@gmail.com>
2023-08-15 20:45:22 +0800

    15 agst pzn alur buat app keep mindatama

--- lib/pages/home/network_page.dart
+++ lib/pages/home/network_page.dart
@@ -23,40 +23,6 @@ class NetworkPage extends StatelessWidget {
     );
   }
 }
-//ngejar 15 jun masih devops
-//ngejar 16 jun masih devops (nutanix)
-//ngejar 17 jun belum nge progress apa apa
-//ngejar 18 jun kerja dulu
-//ngejar 19 jun ikut lomba, menang cuy juara 2
-//20 jun mulai nyatet nutanix, bismillah
-//21 jun lupa buat
-//22 jun hari kamis, sepertinya agak luang
-//23 jun malah  nge scroll duh
-//24 jun bisa dong. gada penghalang!!!!
-//25 jun off malah scroll
-//26 jun kaga ngapa2in, asem
-//27 jun scroll aja,hah
-//28 jun keknya bisa deh
-//29 jun malah scroll
-//30 jun anjing AC apa heater cok. 24 rasa 30, rak adus. mambu su!!
-//2 ju1 jalana jalan ke ip lanjut scrol, elahl
-//3 ju1 bisa deh, kemaren enak sayang dilewatkan. hari ini enak juga.
-//3 ju1 lg
-//4 ju1 mulai lg
-//5 ju1 heleh
-//6 ju1 puffff
-//7 jul 
-//8 jul scrll beli se22
-//9 jul muali nulis yok
-//13 jul mulai lagi
-//14 jul lagi
-//15 jul apa itu nutanix
-//16 jul nutanix
-//20 abal abal
-//7 agustus cok!!
-//8 agst
-//13 agst hanya comment aj
-//14 agst sama comment
-//masih lanjut docker lagi
-//lagi, lagi, lagi, lagi, lagi.lagi
-//lagi, sekarang fokus nutanix.. lagi. membukalagi, lagi. lagi, lagi. lagi, lagi
+
+//15 agst alur kerja pembuatan apps pzn keep mindatama
+
